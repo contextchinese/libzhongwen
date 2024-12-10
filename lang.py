d5154e4a7d2a232b91b817c5037cb7ef3993cb9f
shanhui <adrienshen.dev@gmail.com>
2024-12-09 19:52:06 -0600

    initial commit

---
+++ lang.py
@@ -0,0 +1,21 @@
+
+# to be implemented
+def get_pinyin(sentence: str):
+    pass
+
+
+def identify_hsk_vocab(sentence: str):
+    pass
+
+
+def simplify_complex_sentence(sentence: str):
+    pass
+
+
+def check_grammar(sentence: str):
+    pass
+
+
+def find_similar_sentence(sentence: str):
+    pass
+
