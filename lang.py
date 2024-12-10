bc12ccd66246bbbc6b9eab81b6377e5066392d26
shanhui <adrienshen.dev@gmail.com>
2024-12-09 20:16:31 -0600

    write instructions

--- lang.py
+++ lang.py
@@ -18,4 +18,3 @@ def check_grammar(sentence: str):
 
 def find_similar_sentence(sentence: str):
     pass
-
---
+++ utils.py
@@ -0,0 +1,21 @@
+# utils.py
+from typing import List
+
+'''
+    Difficulty: 1/5
+    Implement locate_character_indexes func so that given sentences: str such as '我爱学习中文'
+        and words: List, such as ['爱'， 中文], the func should return a list of indexes at
+        the words were found in the sentences, the example should return [1, 3] because 爱 and
+        中文 is located at sentence[1] and sentence[3]
+        Note that a prerequisite is splitting up sentences into a word list
+'''
+def locate_character_indexes(sentence: str, words) -> List[int]:
+    pass
+
+
+'''
+    Difficulty: 1/5
+    Given a csv file path, read the file, and parse the contents into a word list: List[int]
+'''
+def read_word_list_csv(file_path: str) -> List[int]:
+    pass
