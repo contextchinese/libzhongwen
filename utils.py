40d3d52ab950b28b9e4af966dc821b4fe5a0de1d
shanhui <adrienshen.dev@gmail.com>
2024-12-09 21:41:31 -0600

    update instructions

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from typing import List
 '''
     Problem #001
     Difficulty: 1/5
-    Implement `locate_character_indexes` func so that given sentences: str such as '我爱学习中文'
+    Implement `locate_character_indexes` func so that given sentences: List such as ['我', '爱', '学习', '中文']
         and words: List, such as ['爱'， 中文], the func should return a list of indexes at
         the words were found in the sentences, the example should return [1, 3] because 爱 and
         中文 is located at sentence[1] and sentence[3]
